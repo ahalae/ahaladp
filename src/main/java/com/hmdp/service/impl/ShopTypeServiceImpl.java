693ef5dbd3951669e0bd26da0fe00731483cf781
AHaLa <taonengyi@hotmail.com>
2024-03-31 18:27:10 +0800

    userLogin

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -17,4 +30,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryTypeList() {
+        //从redis查询缓存
+        String key="typelist";
+        List<String> typeStringList = stringRedisTemplate.opsForList().range(key, 0, stringRedisTemplate.opsForList().size(key));
+
+
+        if (typeStringList!=null&&typeStringList.size()>0) {
+            //存在则返回
+            List<ShopType> shopTypeList = typeStringList.stream().map(o -> JSONUtil.toBean(o, ShopType.class)).collect(Collectors.toList());
+            return Result.ok(shopTypeList);
+        }
+        //不存在，读数据库
+        List<ShopType> shopTypeList = list();
+        //不存在，返回错误
+        if(shopTypeList==null||shopTypeList.size()==0) {
+            return Result.fail("店铺不存在！");
+        }
+        //存在写入redis
+        stringRedisTemplate.opsForList().rightPushAll(key,shopTypeList.stream().map(o->JSONUtil.toJsonStr(o)).collect(Collectors.toList()));
+
+        //返回
+        return Result.ok(shopTypeList);
+    }
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,33 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +37,88 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号
+        if(RegexUtils.isPhoneInvalid(phone)){
+            //不符合返回
+            return Result.fail("手机号格式错误");
+        }
+
+        //生成验证码
+
+        String code = RandomUtil.randomNumbers(6);
+
+        //保存到session
+//        session.setAttribute("code",code);
+
+        //保存到redis
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
+        //发送验证码
+        log.debug("发送验证码成功。验证码：{}",code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号
+        String phone=loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+            //不符合返回
+            return Result.fail("手机号格式错误");
+        }
+        //校验验证码
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
+        String code=loginForm.getCode();
+
+        if(cacheCode==null||!cacheCode.equals(code)){
+            //不一致报错
+            return Result.fail("验证码错误");
+        }
+        //一致查数据库
+        LambdaQueryWrapper<User> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getPhone,phone);
+        User user = getOne(queryWrapper);
+        //用户不存在注册 保存到session
+        if(user==null){
+            user = createUserWithPhone(phone);
+        }
+        //用户存在登录 保存到session
+//        session.setAttribute("user", BeanUtil.copyProperties(user,UserDTO.class));
+
+        //用户存在登录 保存到redis
+        //随机生成token
+        String token = UUID.randomUUID().toString(true);
+        //user->hash
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(), CopyOptions.create().setIgnoreNullValue(true).setFieldValueEditor((fieldName,fieldValue) -> fieldValue.toString()));
+        //存储
+        String tokenKey=LOGIN_USER_KEY+token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
+        //设置有效期
+        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL, TimeUnit.SECONDS);
+        return Result.ok(token);
+    }
+
+    @Override
+    public Result me() {
+        UserDTO user = UserHolder.getUser();
+        return Result.ok(user);
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user=new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        //保存用户
+        save(user);
+        return user;
+    }
 
 }
